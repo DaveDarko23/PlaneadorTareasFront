334c40c6ac594e84c07d3fb946d51132b09f17b3
DaveDarko23 <a22110098@ceti.mx>
2024-05-10 16:24:43 -0600

    Login Estatico

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LoginLibre from "./LoginLibre"
+
+function App() {
+
+
+  return (
+    <>
+      <LoginLibre />
+      <hr />
+    </>
+  )
+}
+
+export default App
---
+++ src/LoginLibre.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+const LoginLibre = () => {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (user.username === "DaveDarko" && user.password === "Comida Rica 123")
+      alert("Estas loggeado en la aplicación... si tan solo hubiera una")
+    else
+      alert("No estás usando las palabras magicas")
+
+    setUser({ username: "", password: "" });
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="username" placeholder="Nombre de usuario" value={user.username} onChange={onChange} />
+        <br />
+        <input type="password" name="password" placeholder="Contraseña" value={user.password} onChange={onChange} />
+        <br />
+        <input type="submit" value="Iniciar Sesión" />
+      </form>
+    </>
+  )
+}
+
+export default LoginLibre
